81044e47349db5d117a759b48ffc0544b4a44b10
koki <ck.kidenda@gmail.com>
2025-05-28 22:55:18 +0300

    new rel added

--- models/user.py
+++ models/user.py
@@ -8,6 +8,9 @@ class User(Base):
     name = Column(String, unique=True, index=True)
     
     # Relationships
-    food_entries = relationship("FoodEntry", back_populates="user")
-    goals = relationship("Goal", back_populates="user", uselist=False)
-    meal_plans = relationship("MealPlan", back_populates="user")
+    food_entries = relationship("FoodEntry", back_populates="user", cascade="all, delete-orphan")
+    goals = relationship("Goal", back_populates="user", uselist=False, cascade="all, delete-orphan")
+    meal_plans = relationship("MealPlan", back_populates="user", cascade="all, delete-orphan")
+    
+    def __repr__(self):
+        return f"<User(id={self.id}, name='{self.name}')>"
